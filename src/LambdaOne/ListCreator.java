398b9d29bbeb5c3735ca5f1438d25498e7d48bd6
Hubert Wielgus <hubert.wielgus@gmail.com>
2020-03-10 12:32:40 +0100

    zrobione 1, 2, 3

--- src/LambdaOne/ListCreator.java
+++ src/LambdaOne/ListCreator.java
@@ -1,53 +1,55 @@
-//package LambdaOne;
-//
-//import java.util.ArrayList;
-//import java.util.Iterator;
-//import java.util.List;
-//
-//public class ListCreator  <T> {
-//
-//    List<T> mylist;
-//
-//    public ListCreator(List<T> list){
-//
-//        mylist = new ArrayList<>(list);
-//
-//    }
-//
-//    public static <T> ListCreator collectFrom(List<T> t){
-//
-//        return new ListCreator<T>(t);
-//    }
-//
-//    public ListCreator<T> when(Selector sel){
-//        Iterator myItr = mylist.iterator();
-//
-//        T element;
-//
-//        while(myItr.hasNext()){
-//
-//            element = (T)myItr.next();
-//
-//            if(!sel.select(element)){
-//                myItr.remove();
-//            }
-//
-//        }
-//        return this;
-//    }
-//
-//    public List<T> mapEvery(Mapper map){
-//
-//        Iterator myItr = mylist.iterator();
-//
-//        T element;
-//
-//        while(myItr.hasNext()){
-//            element = (T)myItr.next();
-//            mylist.set(mylist.indexOf(element), (T)map.map(element));
-//        }
-//
-//        return mylist;
-//    }
-//
-//}
+package LambdaOne;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class ListCreator  <T> {
+
+    List<T> mylist;
+
+    public ListCreator(List<T> list){
+
+        mylist = new ArrayList<>(list);
+
+    }
+
+    public static <T> ListCreator collectFrom(List<T> t){
+
+        return new ListCreator<T>(t);
+    }
+
+    public ListCreator<T> when(Predicate predicate){
+        Iterator myItr = mylist.iterator();
+
+        T element;
+
+        while(myItr.hasNext()){
+
+            element = (T)myItr.next();
+
+            if(!predicate.test(element)){
+                myItr.remove();
+            }
+
+        }
+        return this;
+    }
+
+    public List<T> mapEvery(Function function){
+
+        Iterator myItr = mylist.iterator();
+
+        T element;
+
+        while(myItr.hasNext()){
+            element = (T)myItr.next();
+            mylist.set(mylist.indexOf(element), (T)function.apply(element));
+        }
+
+        return mylist;
+    }
+
+}
--- src/LambdaOne/Main.java
+++ src/LambdaOne/Main.java
@@ -6,11 +6,15 @@ import java.util.List;
 public class Main {
 
     static List<String> getPricesInPLN(List<String> destinations, double xrate) {
-        return null;
+        return ListCreator.collectFrom(destinations)
+                .when( i -> i.toString().split(" ")[0].matches("WAW")
+                )
+                .mapEvery( i ->   "to " + i.toString().split(" ")[1] + " - price in PLN: " + (int)(Integer.parseInt(i.toString().split(" ")[2])*xrate)
+
+                );
     }
 
     public static void main(String[] args) {
-
         // Lista destynacji: port_wylotu port_przylotu cena_EUR
         List<String> dest = Arrays.asList(
                 "bleble bleble 2000",
@@ -19,10 +23,8 @@ public class Main {
                 "WAW DPS 2000",
                 "WAW HKT 1000"
         );
-
         double ratePLNvsEUR = 4.30;
         List<String> result = getPricesInPLN(dest, ratePLNvsEUR);
         for (String r : result) System.out.println(r);
-
     }
 }
